4c0fcb8a724f2e21c6d243e483742e4ecb1e47e5
nbarredo <nbarredo@techietalent.net>
2025-05-14 17:50:24 -0300

    add system

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'SharedUI',
       fileName: (format) => `shared-ui.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ['system']
     },
     rollupOptions: {
       // Make sure to externalize deps that shouldn't be bundled
