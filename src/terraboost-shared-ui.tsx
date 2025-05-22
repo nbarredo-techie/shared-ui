2d9d84c4d7b0378b60944cfe9622901e147bf4f9
nbarredo <nbarredo@techietalent.net>
2025-05-22 15:00:01 -0300

    adding css

--- src/terraboost-shared-ui.tsx
+++ src/terraboost-shared-ui.tsx
@@ -1,3 +1,5 @@
+import "./output.css"; // Or potentially './input.css' if your webpack setup processes it further
+
 // Anything exported from this file is importable by other in-browser modules.
 export function publicApiFunction() {}
 export { Button } from "./components/ui/button";
