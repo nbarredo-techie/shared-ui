4f8f0e71a1eae16e430a7d47d691734f85077fa3
nbarredo <nbarredo@techietalent.net>
2025-05-26 15:03:46 -0300

    adding dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // Added for class-based dark mode
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
